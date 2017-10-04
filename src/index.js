c1096f2e3fe8c3fd2f40659a03985ff14dc7625e
Colin Aulds <caulds989@gmail.com>
2017-10-04 13:46:33 -0700

    render search bar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-//Go get react from the modules
-
+//Go get react and react-dom from the modules
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCmtXy1ADvcwfOrgyvjIKvka7bx8x7Y7EA';
+
+
 
 // Create a new component. this component should produce some HTML
-
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );  
 }
 
 // Take this component's generated HTML and put on the pafe (in the DOM).
-
 ReactDOM.render(
 <App />, document.querySelector('.container'));
