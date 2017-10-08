4e4c231bab43628dbce4b1f883c54a8e3c003935
Colin Aulds <caulds989@gmail.com>
2017-10-08 13:34:31 -0700

    Add proper api key, add list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 //Go get react and react-dom from the modules
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyAJ1oSoly1GcKqHCzMAZ931dHy4u940js0';
 
-const API_KEY = 'AIzaSyB1pJrx9H6T6M4S2u07Uhqf_2GjqiFLgC8';
 
 // Create a new component. this component should produce some HTML
-const App = () => {
+class App extends Component {
+  constructor (props) {
+    super (props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      //ES6 syntax: if key and value are the same, you can just leave the key name. 
+      //Same as this.setState({ videos:videos });
+    });
+  }
+  render () {
     return (
       <div>
         <SearchBar />
+        <VideoList />
       </div>
-    );  
+    );
+  }  
 }
 
-// Take this component's generated HTML and put on the pafe (in the DOM).
+// Take this component's generated HTML and put on the page (in the DOM).
 ReactDOM.render(
 <App />, document.querySelector('.container'));
